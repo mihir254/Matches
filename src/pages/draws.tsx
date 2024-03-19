a0d800cf8d9fa54ccc50f7d60c5b3b485efde63f
Mihir Bhansali <mbmihirbhansali@gmail.com>
2024-03-19 16:41:59 -0400

    added sample teams

--- src/pages/draws.tsx
+++ src/pages/draws.tsx
@@ -107,6 +107,10 @@ const Draws = () => {
         return matches;
     };
     
+    const addRandomTeams = (numberOfTeams: number) => {
+        const newTeams = Array.from({ length:numberOfTeams }, (_, i) => `Team ${i + 1}`);
+        setTeams(newTeams);
+    }
 
     return (
         <Flex direction={"column"} gap={"40px"} width={"100%"} alignItems={"center"}>
@@ -114,11 +118,17 @@ const Draws = () => {
                 <Input width={"200px"} name="team" value={team} onChange={(event: ChangeEvent<HTMLInputElement>) => setTeam(event.target.value)}/>
                 <Button isDisabled={team === ""} onClick={saveTeam}>Add Team</Button>
             </Flex>
-            <Flex gap={"7px"} maxWidth={{base:"90%", md: "50%"}} wrap={"wrap"} justifyContent={"center"}>
+            {teams.length === 0 && <Flex gap={"7px"}>
+                <Button onClick={() => addRandomTeams(10)}>Add 10 Teams</Button>
+                <Button onClick={() => addRandomTeams(16)}>Add 16 Teams</Button>
+                <Button onClick={() => addRandomTeams(20)}>Add 20 Teams</Button>
+            </Flex>}
+            {teams.length > 0 && <Flex gap={"7px"} maxWidth={{base:"90%", md: "50%"}} wrap={"wrap"} justifyContent={"center"}>
                 {teams.map((team, index) => (
                     <Button key={index} onClick={() => setTeams(prev => prev.filter(t => t !== team))}>{team} <IoIosClose/></Button>
                 ))}
-            </Flex>
+                <Button colorScheme='red' onClick={() => {setTeams([]); setgroups(1); setCustom(0); setLots([])}}>Clear</Button>
+            </Flex>}
             <Heading size={{base: "sm", lg: "lg"}}>TOTAL TEAMS : { teams.length }</Heading>
             <Flex gap={"10px"} alignItems={"center"}>
                 <Text>Groups: </Text>
@@ -131,7 +141,7 @@ const Draws = () => {
                 <Button isDisabled={teams.length === 0} onClick={() => setLots(createRoundRobinMatches())}>Create Round Robin Matches</Button>
                 <Button isDisabled={teams.length === 0} onClick={() => setLots(createCustomRoundRobinMatches())}>Create Custom Round Robin Matches</Button>
             </Flex>
-            {lots && lots.length > 0 && <Flex gap={"7px"} bgColor="whitesmoke" rounded={"md"} p={{base: 5, lg: 10}} shadow={"sm"}
+            {teams.length > 0 && lots && lots.length > 0 && <Flex gap={"7px"} bgColor="whitesmoke" rounded={"md"} p={{base: 5, lg: 10}} shadow={"sm"}
                 maxWidth={{base:"90%", md: "50%"}} wrap={"wrap"}>
                 {lots.map((lot, ind) => (
                     <Flex gap={"7px"} key={ind} wrap={"wrap"} alignItems={"center"} justifyContent={"center"}>
